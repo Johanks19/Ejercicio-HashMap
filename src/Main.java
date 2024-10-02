bb46805fa64e96721a8ca0249f53f16ba2f8e599
JOHAN BLANCO <johansneyder.bs19@gmail.com>
2024-10-01 21:12:28 -0400

    cambio

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,59 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Johan Sneyder Blanco Sanchez
+// id: 936056
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Crear un HashMap para almacenar los pokemones disponibles para adopción
+        HashMap<Integer, Pokemon> pokemonesDisponibles = new HashMap<>();
+        pokemonesDisponibles.put(1, new Pokemon("Pikachu", "Eléctrico"));
+        pokemonesDisponibles.put(2, new Pokemon("Charmander", "Fuego"));
+        pokemonesDisponibles.put(3, new Pokemon("Bulbasaur", "Planta"));
+        pokemonesDisponibles.put(4, new Pokemon("Squirtle", "Agua"));
+        pokemonesDisponibles.put(5, new Pokemon("Eevee", "Normal"));
+
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("\n*** Centro de Adopción de Pokemones ***");
+            System.out.println("1. Ver pokemones disponibles");
+            System.out.println("2. Adoptar un pokemon");
+            System.out.println("3. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("\nPokemones disponibles para adopción:");
+                    for (Integer id : pokemonesDisponibles.keySet()) {
+                        System.out.println("ID: " + id + " - " + pokemonesDisponibles.get(id));
+                    }
+                    break;
+                case 2:
+                    System.out.print("\nIngrese el ID del pokemon que desea adoptar: ");
+                    int id = scanner.nextInt();
+
+                    if (pokemonesDisponibles.containsKey(id)) {
+                        System.out.println("¡Felicidades! Has adoptado a " + pokemonesDisponibles.get(id).getNombre());
+                        pokemonesDisponibles.remove(id);
+                    } else {
+                        System.out.println("ID inválido o el pokemon ya ha sido adoptado.");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Gracias por visitar el Centro de Adopción de Pokemones. ¡Hasta pronto!");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente de nuevo.");
+            }
+        } while (opcion != 3);
+
+        scanner.close();
+
+
     }
-}
+
+    }
--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
-
 public class Pokemon {
+    private String nombre;
+    private String elemento;
+
+    public Pokemon(String nombre, String elemento) {
+        this.nombre = nombre;
+        this.elemento = elemento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(String elemento) {
+        this.elemento = elemento;
+    }
+
+    @Override
+    public String toString() {
+        return "Pokemon{" +
+                "nombre='" + nombre + '\'' +
+                ", elemento='" + elemento + '\'' +
+                '}';
+    }
 }
